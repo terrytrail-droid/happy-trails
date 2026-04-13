e64604d94e45817590d1fe1ae2833e8e88207c11
terrytrail-droid <terrynaas@gmail.com>
2026-04-13 17:40:51 -0400

    Update recipes.js

--- api/recipes.js
+++ api/recipes.js
@@ -1,1 +1,9 @@
-recipes.js
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  const response = await fetch(
+    `https://api.airtable.com/v0/${process.env.AIRTABLE_BASE}/${process.env.AIRTABLE_TABLE}`,
+    { headers: { Authorization: `Bearer ${process.env.AIRTABLE_TOKEN}` } }
+  );
+  const data = await response.json();
+  res.status(200).json(data);
+}
